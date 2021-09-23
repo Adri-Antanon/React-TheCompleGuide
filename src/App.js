f7580966f3e4d3f1e7c92ca4023db7bbc677415b
Adrian Antanon Orozco <aantanon@brothapps.com>
2021-09-24 01:24:59 +0200

    feat: :zap: add normal date logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import { ExpenseItem } from "./components";
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Car Insurance",
+      amount: 298.21,
+      date: new Date(2021, Math.random() * 12, Math.random() * 28),
+    },
+    {
+      id: 2,
+      title: "Car Insurance",
+      amount: 298.21,
+      date: new Date(2021, Math.random() * 12, Math.random() * 28),
+    },
+    {
+      id: 3,
+      title: "Car Insurance",
+      amount: 298.21,
+      date: new Date(2021, Math.random() * 12, Math.random() * 28),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>This is also visible!</p>
-      <ExpenseItem title="Expense Item" date="21 SEP, 2021" amount="17.99" />
+      {expenses.map((expense) => (
+        <ExpenseItem
+          amount={expense.amount}
+          key={expense.id}
+          title={expense.title}
+          date={expense.date}
+        />
+      ))}
     </div>
   );
 }
