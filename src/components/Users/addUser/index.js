acc218c25a610e01f03362a208215068dc3e058b
Adrian Antanon Orozco <aantanon@brothapps.com>
2021-09-28 11:11:54 +0200

    feat: :fire: manage the user input state

--- src/components/Users/addUser/index.js
+++ src/components/Users/addUser/index.js
@@ -1,18 +1,27 @@
+import { useRef } from "react";
 import { Card, Button } from "../../UI";
 
 import classes from "./styles.module.css";
 
 export const AddUser = (props) => {
+  const usernameInput = useRef();
+  const ageInput = useRef();
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    const enteredUsername = usernameInput.current.value;
+    const enteredAge = ageInput.current.value;
+
+    console.log(enteredUsername, enteredAge);
   };
+
   return (
     <Card classname={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input type="text" name="username" id="username" ref={usernameInput} />
         <label htmlFor="age">Age (Years)</label>
-        <input type="text" name="age" id="age" />
+        <input type="text" name="age" id="age" ref={ageInput} />
         <Button type="submit">Add User</Button>
       </form>
     </Card>
