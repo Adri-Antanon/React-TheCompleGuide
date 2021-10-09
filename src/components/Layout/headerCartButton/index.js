eb01b90255c988b8f0b34bd069431120c1fdbb0e
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-10-09 17:17:45 +0200

    feat: :gem: using the context

--- src/components/Layout/headerCartButton/index.js
+++ src/components/Layout/headerCartButton/index.js
@@ -1,15 +1,24 @@
+import { useContext } from "react";
+
 import { CartIcon } from "../../Cart";
+import CartContext from "../../../store/cart-context";
 
 import classes from "./styles.module.css";
 
 export const HeaderCartButton = ({ onClick }) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((accumulator, item) => {
+    return accumulator + item.amount;
+  }, 0);
+
   return (
     <button onClick={onClick} className={classes.button}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
