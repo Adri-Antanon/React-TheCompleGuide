969a25c20693479b6b02b21dc4f1dc646afd65c7
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-10-11 17:39:22 +0200

    feat: :robot: making items removable

--- src/components/Cart/cart/index.js
+++ src/components/Cart/cart/index.js
@@ -12,8 +12,12 @@ export const Cart = ({ onClose }) => {
   const totalAmount = `${cartCtx.totalAmount.toFixed(2)}€`;
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
-  const cartItemAddHandler = (item) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
 
   const cartItems = (
     <ul className={classes["cart-items"]}>
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -12,7 +12,7 @@ const REMOVE = "REMOVE";
 
 const cartReducer = (state, action) => {
   switch (action.type) {
-    case ADD:
+    case ADD: {
       const updatedTotalAmount =
         state.totalAmount + action.item.price * action.item.amount;
 
@@ -38,7 +38,33 @@ const cartReducer = (state, action) => {
         items: updatedItems,
         totalAmount: updatedTotalAmount,
       };
-    case REMOVE:
+    }
+
+    case REMOVE: {
+      const existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.id
+      );
+      const existingItem = state.items[existingCartItemIndex];
+      const updatedTotalAmount = state.totalAmount - existingItem.price;
+      let updatedItems;
+
+      if (existingItem.amount === 1) {
+        updatedItems = state.items.filter((item) => item.id !== action.id);
+      } else {
+        const updatedItem = {
+          ...existingItem,
+          amount: existingItem.amount - 1,
+        };
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    }
+
     default:
       return defaultCartState;
   }
