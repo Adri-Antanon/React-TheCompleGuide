8056d661eb21ae55fb47d8dbbd283554abf83985
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-10-19 16:02:21 +0200

    refactor: :robot: using async await

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -6,24 +6,18 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  useEffect(() => {}, []);
-
-  function fetchMoviesHandler() {
-    fetch("https://swapi.tech/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    const response = await fetch("https://www.swapi.dev/api/films");
+    const data = response.json();
+    const transformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
