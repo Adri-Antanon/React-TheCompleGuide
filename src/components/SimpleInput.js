d1cf32ca18db692aea348d3fa19ff83889989add
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-10-26 13:39:37 +0200

    feat: :lock: adding basic validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,33 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState("");
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    if (enteredName.trim() === "") {
+      return;
+    }
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    setEnteredName("");
+  };
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          value={enteredName}
+          onChange={nameInputChangeHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
