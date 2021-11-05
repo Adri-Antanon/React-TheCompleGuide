dce80907f9d32b696c5448808bc556dd7411e43a
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-11-05 11:45:06 +0100

    feat: :wrench: dispatching actions

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,12 @@ import { createStore } from "redux";
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
 
-const counterReducer = (state = 0, action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
   switch (action.type) {
     case INCREMENT:
-      return { state: state.counter + 1 };
+      return { counter: state.counter + 1 };
     case DECREMENT:
-      return { state: state.counter - 1 };
+      return { counter: state.counter - 1 };
     default:
       return state;
   }
