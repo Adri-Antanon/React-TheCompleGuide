9bbb9ed010ed1f95fdf9203dd60c2d902b34ea6e
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-11-06 16:57:34 +0100

    feat: :robot: working with multiple state properties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,12 @@
 import { useSelector, useDispatch } from "react-redux";
-import { DECREMENT, INCREMENT, INCREASE } from "../store";
+import { DECREMENT, INCREMENT, INCREASE, TOGGLE } from "../store";
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: INCREMENT });
@@ -16,12 +17,14 @@ const Counter = () => {
   const decrementHandler = () => {
     dispatch({ type: DECREMENT });
   };
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: TOGGLE });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increse by 5</button>
--- src/store/index.js
+++ src/store/index.js
@@ -3,15 +3,26 @@ import { createStore } from "redux";
 export const INCREMENT = "INCREMENT";
 export const DECREMENT = "DECREMENT";
 export const INCREASE = "INCREASE";
+export const TOGGLE = "TOGGLE";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case INCREMENT:
-      return { counter: state.counter + 1 };
+      return { counter: state.counter + 1, showCounter: state.showCounter };
     case DECREMENT:
-      return { counter: state.counter - 1 };
+      return { counter: state.counter - 1, showCounter: state.showCounter };
     case INCREASE:
-      return { counter: state.counter + action.amount };
+      return {
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
+      };
+    case TOGGLE:
+      return {
+        showCounter: !state.showCounter,
+        counter: state.counter,
+      };
     default:
       return state;
   }
