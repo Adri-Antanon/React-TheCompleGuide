15077589c273786809b670b1c0521baeaf626fea
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-11-07 11:48:53 +0100

    feat: :alien: starting project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+
 function App() {
-  return <h1>React - The Complete Guide</h1>;
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
 }
 
 export default App;
