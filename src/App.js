dce9f0902c156cb959087df83be357230dd29471
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-11-13 11:39:51 +0100

    feat: :zap: add react-router & define routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, Switch } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+
 function App() {
-  return <h1>React - The Complete Guide</h1>;
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route exact path="/welcome">
+            <Welcome />
+          </Route>
+
+          <Route path="/products">
+            <Products />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
