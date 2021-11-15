9cded1c99cf310dd1ebc14d1009d54982634c958
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-11-15 12:45:40 +0100

    feat: add dummy data & content

---
+++ src/config/constants.js
@@ -0,0 +1,13 @@
+export const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Adri",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Andera",
+    text: "Learning Next is fun!",
+  },
+  { id: "q3", author: "Dani", text: "Learning JS is fun!" },
+];
--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,9 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+import { DUMMY_QUOTES } from "../config/constants";
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
