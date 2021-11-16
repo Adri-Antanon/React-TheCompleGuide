c74373e0b1af47c1ac62acb1a9d45660ae8aeb5c
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-11-16 11:11:39 +0100

    feat: :sparkles: add NotFound page & outputting data

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,10 +1,21 @@
 import { useParams, Route } from "react-router-dom";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+import { DUMMY_QUOTES } from "../config/constants";
+
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <div>
-      <h1>Quote detail Page</h1>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <p>{params.quoteId}</p>
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
