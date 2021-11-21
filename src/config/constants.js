d1eacd2d546b0c45a7919bf5087be7869814551f
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-11-21 12:00:19 +0100

    feat: :lock: add user signup

---
+++ src/config/constants.js
@@ -0,0 +1,6 @@
+export const FIREBASE_URL = "";
+
+export const FIREBASE_SIGNUP =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=";
+
+export const API_KEY = "AIzaSyDIdI9oreOoSFgMUR2gylkl2nkw-gv-t78";
