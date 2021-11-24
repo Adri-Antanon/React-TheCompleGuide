f56b78c69e89625977f44e299bd6af6fe12d66ba
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-11-24 10:33:37 +0100

    refactor: update next config

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,12 +1,14 @@
-import Card from '../ui/Card';
-import classes from './MeetupItem.module.css';
+import Image from "next/image";
+
+import Card from "../ui/Card";
+import classes from "./MeetupItem.module.css";
 
 function MeetupItem(props) {
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img src={props.image} alt={props.title} />
+          <Image layout="fill" src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
--- next.config.js
+++ next.config.js
@@ -1,3 +1,32 @@
-module.exports = {
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+const imageConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["upload.wikimedia.org"],
+  },
+};
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      ...imageConfig,
+      env: {
+        mongodb_username: "Adri",
+        mongodb_password: "Contra123",
+        mongodb_clustername: "nextjscluster",
+        mongodb_database: "meetups-dev",
+      },
+    };
+  }
+
+  return {
+    ...imageConfig,
+    env: {
+      mongodb_username: "Adri",
+      mongodb_password: "Contra123",
+      mongodb_clustername: "nextjscluster",
+      mongodb_database: "meetups",
+    },
+  };
+};
