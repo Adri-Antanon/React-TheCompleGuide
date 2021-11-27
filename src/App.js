678bcd73d3c983a5f5f59651833d23355e144b04
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-11-27 20:50:57 +0100

    feat: :star: add starting setup project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
-function App() {
-  return <h1>React - The Complete Guide</h1>;
-}
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import Navigation from './components/Nav/Navigation';
+import ProductsPage from './containers/Products';
+import FavoritesPage from './containers/Favorites';
+
+const App = props => {
+  return (
+    <React.Fragment>
+      <Navigation />
+      <main>
+        <Route path="/" component={ProductsPage} exact />
+        <Route path="/favorites" component={FavoritesPage} />
+      </main>
+    </React.Fragment>
+  );
+};
 
 export default App;
