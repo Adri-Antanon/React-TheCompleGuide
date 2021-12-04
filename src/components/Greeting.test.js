de766f8eb080ae89d5c162ee1f71fe95b1f92a00
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-12-04 18:10:04 +0100

    test: :test_tube: writing first text

---
+++ src/components/Greeting.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import Greeting from "./Greeting";
+
+test("renders Hello World as a text ", () => {
+  //Arrange
+  render(<Greeting />);
+  // Act
+  // Nothing for now...
+  // Assert
+  const helloWorldElement = screen.getByText("Hello World!");
+
+  expect(helloWorldElement).toBeInTheDocument();
+});
