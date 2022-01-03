e7b4f85beb8d47cc611bf370b8199cbf8994194a
Adrian Antanon Orozco <adrianantanon@gmail.com>
2022-01-03 11:51:25 +0100

    feat: add useContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import Ingredients from './components/Ingredients/Ingredients';
+import Ingredients from "./components/Ingredients/Ingredients";
+import Auth from "./components/Auth";
+import { AuthContext } from "./context/auth-context";
 
-const App = props => {
-  return <Ingredients />;
+const App = (props) => {
+  const AuthCtx = useContext(AuthContext);
+
+  let content = AuthCtx.isAuth ? <Ingredients /> : <Auth />;
+
+  return content;
 };
 
 export default App;
